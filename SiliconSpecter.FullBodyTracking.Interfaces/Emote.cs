edd299b5aa0d05735a79924e7cf2865eff12fd02
siliconspecter <siliconspecter@outlook.com>
2022-10-04 00:23:31 +0100

    Remove unused import.

--- SiliconSpecter.FullBodyTracking.Interfaces/Emote.cs
+++ SiliconSpecter.FullBodyTracking.Interfaces/Emote.cs
@@ -1,31 +1,29 @@
-﻿using System.Numerics;
-
-namespace SiliconSpecter.FullBodyTracking.Interfaces
+﻿namespace SiliconSpecter.FullBodyTracking.Interfaces
 {
+  /// <summary>
+  /// The emote currently being displayed by a <see cref="Player"/>.
+  /// </summary>
+  public enum Emote
+  {
     /// <summary>
-    /// The emote currently being displayed by a <see cref="Player"/>.
+    /// The <see cref="Player"/> is not currently making any particular emote.
     /// </summary>
-    public enum Emote
-    {
-        /// <summary>
-        /// The <see cref="Player"/> is not currently making any particular emote.
-        /// </summary>
-        Neutral,
+    Neutral,
 
-        /// <summary>
-        /// The <see cref="Player"/> is currently making an angry emote.
-        /// </summary>
-        Angry,
+    /// <summary>
+    /// The <see cref="Player"/> is currently making an angry emote.
+    /// </summary>
+    Angry,
 
-        /// <summary>
-        /// The <see cref="Player"/> is currently making a surprised emote.
-        /// </summary>
-        Surprised,
+    /// <summary>
+    /// The <see cref="Player"/> is currently making a surprised emote.
+    /// </summary>
+    Surprised,
 
-        /// <summary>
-        /// The <see cref="Player"/> is currently making a joy (smiling) emote.
-        /// </summary>
-        //
-        Joy,
-    }
+    /// <summary>
+    /// The <see cref="Player"/> is currently making a joy (smiling) emote.
+    /// </summary>
+    //
+    Joy,
+  }
 }
