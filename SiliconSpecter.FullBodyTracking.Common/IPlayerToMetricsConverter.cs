8444e208a5cda1660bf1ef99ba1aa29d540dfdbb
siliconspecter <siliconspecter@outlook.com>
2022-10-04 18:50:47 +0100

    Fix incorrect type.

--- SiliconSpecter.FullBodyTracking.Common/IPlayerToMetricsConverter.cs
+++ SiliconSpecter.FullBodyTracking.Common/IPlayerToMetricsConverter.cs
@@ -14,6 +14,6 @@ namespace SiliconSpecter.FullBodyTracking.Common
     /// <param name="metrics">The previous <see cref="Metrics"/>, if any, otherwise, <see langword="null"/>.</param>
     /// <returns>The <see cref="Metrics"/> converted from the given <paramref name="player"/>.</returns>
     /// <exception cref="Exception">Thrown when <see cref="Player.Details"/> is <see langword="null"/>.</exception>
-    InterpolatablePlayerKeyframe Convert(Player player, Metrics? metrics);
+    Metrics Convert(Player player, Metrics? metrics);
   }
 }
