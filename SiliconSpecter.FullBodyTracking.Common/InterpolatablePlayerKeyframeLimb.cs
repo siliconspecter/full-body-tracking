78e1628eb8a4eaaed762eecbb97672c68e26ab30
siliconspecter <siliconspecter@outlook.com>
2022-10-04 20:14:56 +0100

    Specify space.

--- SiliconSpecter.FullBodyTracking.Common/InterpolatablePlayerKeyframeLimb.cs
+++ SiliconSpecter.FullBodyTracking.Common/InterpolatablePlayerKeyframeLimb.cs
@@ -18,12 +18,12 @@ namespace SiliconSpecter.FullBodyTracking.Common
     public float ExtensionProportion;
 
     /// <summary>
-    /// A normal pointing in the direction the limb is bent (e.g. from the midpoint between the shoulder and wrist to the elbow).
+    /// A normal pointing in the direction the limb is bent (e.g. from the midpoint between the shoulder and wrist to the elbow), relative to the facing rotation.
     /// </summary>
     public Vector3 BendNormal;
 
     /// <summary>
-    /// A normal pointing in the direction the tip points (e.g. from the wrist to the tip of the middle finger).
+    /// A normal pointing in the direction the tip points (e.g. from the wrist to the tip of the middle finger), relative to the facing rotation.
     /// </summary>
     public Vector3 TipNormal;
   }
